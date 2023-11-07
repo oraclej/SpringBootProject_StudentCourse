aeb79b3da2a6a1bfba5bc8aba627decc5f487d8d
oraclej <oracleman7@gmail.com>
2023-11-07 15:22:00 +0330

    Final OK

--- src/main/java/ir/oraclej/springbootproject_studentcourse/SpringBootProjectStudentCourseApplication.java
+++ src/main/java/ir/oraclej/springbootproject_studentcourse/SpringBootProjectStudentCourseApplication.java
@@ -2,6 +2,8 @@ package ir.oraclej.springbootproject_studentcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +16,10 @@ public class SpringBootProjectStudentCourseApplication implements WebMvcConfigur
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/admin/studentList");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
